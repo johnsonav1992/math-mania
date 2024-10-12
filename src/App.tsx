494310d6a41e0ace556a34b2ac7f04c3c93a59bc
Alex Johnson <johnsonav1992@gmail.com>
2024-10-12 06:09:37 -0500

    favicon and title

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,16 @@ import * as stylex from "@stylexjs/stylex";
 import { colors } from "./styles/theme.stylex";
 import { style } from "./styles/utils";
 
+function App() {
+  return (
+    <div {...style(styles.bg)}>
+      <h1 {...style(styles.title)}>Math Mania!</h1>
+    </div>
+  );
+}
+
+export default App;
+
 const styles = stylex.create({
   bg: {
     backgroundColor: colors.bgBlue,
@@ -18,13 +28,3 @@ const styles = stylex.create({
     fontFamily: "Arial, sans-serif",
   },
 });
-
-function App() {
-  return (
-    <div {...style(styles.bg)}>
-      <h1 {...style(styles.title)}>Math Mania!</h1>
-    </div>
-  );
-}
-
-export default App;
