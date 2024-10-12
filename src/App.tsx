e8332f0663e6978343e5febc69ceab4fa8d49dc7
Alex Johnson <johnsonav1992@gmail.com>
2024-10-12 05:57:24 -0500

    stylex set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import * as stylex from "@stylexjs/stylex";
+import { colors } from "./styles/theme.stylex";
+
+const styles = stylex.create({
+  background: {
+    backgroundColor: colors.bgBlue,
+    height: "100vh",
+    width: "100vw",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
 function App() {
-  return <>'hello'</>;
+  return <div {...stylex.props(styles.background)}>hello</div>;
 }
 
 export default App;
---
+++ src/styles/theme.stylex.ts
@@ -0,0 +1,5 @@
+import * as stylex from "@stylexjs/stylex";
+
+export const colors = stylex.defineVars({
+  bgBlue: "#49c7e7",
+});
