10440b6198d7322ed5c1fd5213ed9392b2b703ac
Alex Johnson <johnsonav1992@gmail.com>
2025-01-04 06:28:26 -0600

    rip out stylex for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import * as stylex from "@stylexjs/stylex";
-import { commonColors } from "./styles/theme.stylex";
-import { style } from "./styles/utils";
-import Gameboard from "./components/Gameboard/Gameboard";
+import Gameboard from './components/Gameboard/Gameboard';
 
 function App() {
   return (
-    <div {...style(styles.bg)}>
-      <h1 {...style(styles.title)}>Math Mania!</h1>
+    <div>
+      <h1>Math Mania!</h1>
       <Gameboard />
     </div>
   );
@@ -14,20 +11,20 @@ function App() {
 
 export default App;
 
-const styles = stylex.create({
-  bg: {
-    backgroundColor: commonColors.bgBlue,
-    height: "100vh",
-    width: "100vw",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  title: {
-    color: "white",
-    fontSize: "5rem",
-    fontWeight: "bold",
-    fontFamily: "Arial, sans-serif",
-  },
-});
+// const styles = stylex.create({
+//   bg: {
+//     backgroundColor: commonColors.bgBlue,
+//     height: "100vh",
+//     width: "100vw",
+//     display: "flex",
+//     flexDirection: "column",
+//     justifyContent: "center",
+//     alignItems: "center",
+//   },
+//   title: {
+//     color: "white",
+//     fontSize: "5rem",
+//     fontWeight: "bold",
+//     fontFamily: "Arial, sans-serif",
+//   },
+// });
--- src/components/Block/Block.tsx
+++ src/components/Block/Block.tsx
@@ -1,26 +1,24 @@
-import { style } from "../../styles/utils";
-import * as stylex from "@stylexjs/stylex";
-import { BlockColors } from "../../types/types";
-import { blockColors } from "../../styles/theme.stylex";
+import { BlockColors } from '../../types/types';
+// import { blockColors } from '../../styles/theme.stylex';
 
 interface Props {
   color: BlockColors;
 }
 
 const Block = ({ color }: Props) => {
-  return <div {...style(styles.numberBlock(color))}></div>;
+  return <div></div>;
 };
 
 export default Block;
 
-const styles = stylex.create({
-  numberBlock: (color: BlockColors) => ({
-    backgroundColor: blockColors[color],
-    borderColor: null,
-    height: "100px",
-    width: "100px",
-    borderRadius: "10px",
-    margin: "10px",
-    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2), 0 6px 20px rgba(0, 0, 0, 0.2)",
-  }),
-});
+// const styles = stylex.create({
+//   numberBlock: (color: BlockColors) => ({
+//     backgroundColor: blockColors[color],
+//     borderColor: null,
+//     height: '100px',
+//     width: '100px',
+//     borderRadius: '10px',
+//     margin: '10px',
+//     boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2), 0 6px 20px rgba(0, 0, 0, 0.2)'
+//   })
+// });
